42bc67897617ee33e6eeff94c7abfaab3f5ee11c
Adonis C. Silveira <desenvolvimento777@gmail.com>
2026-05-01 15:31:36 -0300

    Exemplos de herança multipla

---
+++ modulo_2/heranca_multipla.py
@@ -0,0 +1,32 @@
+class Animal:
+    def __init__(self, nome) -> None:
+        self.nome = nome
+
+    def emitir_som(self):
+        pass
+
+
+class Mamifero(Animal):
+    def amamentar(self):
+        return f"{self.nome} está amamentando."
+
+
+class Ave(Animal):
+    def voar(self):
+        return f"{self.nome} está voando."
+
+
+class Morcego(Mamifero, Ave):
+    def emitir_som(self):
+        return "Morcegos emitem sons ultrassônicos"
+
+
+morcego = Morcego(nome="Batman")
+
+# Acessando métodos da classe base
+print(f"Nome do morcego: {morcego.nome}")
+print(f"Som do morcego: {morcego.emitir_som()}")
+
+# Acessando métodos das classes
+print(f"Morcego amamentando: {morcego.amamentar()}")
+print(f"Morcego voando: {morcego.voar()}")

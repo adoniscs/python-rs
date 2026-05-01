76728e32d65b029c8279591c30f72c5a26b01d03
Adonis C. Silveira <desenvolvimento777@gmail.com>
2026-05-01 13:10:10 -0300

    exemplos de encapsulamento

--- modulo_2/pilares_poo.py
+++ modulo_2/pilares_poo.py
@@ -39,3 +39,27 @@ print("\nExemplo de encapsulamento:")
 class ContaBancaria:
     def __init__(self, saldo) -> None:
         self.__saldo = saldo  # Atributo privado
+
+    def depositar(self, valor):
+        if valor > 0:
+            self.__saldo += valor
+
+    def sacar(self, valor):
+        if valor > 0 and valor <= self.__saldo:
+            self.__saldo -= valor
+
+    def consultar_saldo(self):
+        return self.__saldo
+
+
+conta = ContaBancaria(saldo=1000)
+print(f"Saldo da conta bancária: {conta.consultar_saldo()}")
+conta.depositar(valor=500)
+print(f"Saldo da conta bancária: {conta.consultar_saldo()}")
+conta.depositar(valor=-500)
+print(f"Saldo da conta bancária: {conta.consultar_saldo()}")
+conta.sacar(valor=2200)
+print(f"Saldo da conta bancária: {conta.consultar_saldo()}")
+
+conta_da_joana = ContaBancaria(saldo=100)
+print(f"Saldo da conta bancária da Joana: {conta_da_joana.consultar_saldo()}")
